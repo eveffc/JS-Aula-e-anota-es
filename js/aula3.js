2788862326381b8878eed4eba881e9ee28ca541f
Evelyn Santos <117581658+eveffc@users.noreply.github.com>
2023-03-08 09:11:02 -0300

    Add files via upload

---
+++ js/aula3.js
@@ -0,0 +1,10 @@
+/*let a,b,c; 
+a=5; 
+b=6; 
+c=7;
+
+let a,b,c; a=5; b=6; c=7;*/
+
+Espaços vazios:
+let pessoa = "dimitri" ;
+let pessoa="dimitri";
---
+++ js/aula4.js
@@ -0,0 +1,6 @@
+var a=1;
+var b=7;
+var c=a+b;
+
+alert(c);
+alert('c com as aspas');
